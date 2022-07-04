16e49f6159aec4a61aafb0f65a2a66fbd7d80c3f
daonYoon <zfydz@naver.com>
2022-07-04 17:24:26 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from "apollo-server";
+
+
+const typeDefs = gql`
+type User {
+    id: ID
+    username: String
+}
+
+type Tweet {
+    id:ID
+    text: String
+    author: User
+}
+    type Query {
+        allTweets: [Tweet]
+        tweet(id: ID): Tweet
+    }
+`;
+
+//SERVER OPEN 서버에게 어떤 데이터 형식을 사용할껀지 말해줘야함
+const server = new ApolloServer({typeDefs})
+
+server.listen().then(({url}) => {
+    console.log(`Running on ${url}`)
+})
